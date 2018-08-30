d014c2833db4c23c4fa6b51074dfe9f968e63673
z0976190100 <z0976190100@gmail.com>
2018-08-30 04:19:32 +0300

    init commit

---
+++ src/main/java/com/z0976190100/sock/SockApplication.java
@@ -0,0 +1,12 @@
+package com.z0976190100.sock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockApplication.class, args);
+    }
+}
---
+++ src/test/java/com/z0976190100/sock/SockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.z0976190100.sock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
